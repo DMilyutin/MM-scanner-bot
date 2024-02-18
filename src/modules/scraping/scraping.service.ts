aad4855ce46b10b455378daff77418978ba353d3
Dmitry Milyutin <dima.milyutin.1996@gmail.com>
2024-02-18 18:35:59 +0300

    старт-2

--- src/modules/scraping/scraping.service.ts
+++ src/modules/scraping/scraping.service.ts
@@ -14,6 +14,7 @@ export class ScrapingService{
         const browser = await puppeteer.launch({
             headless: false,
             //args: ['--proxy-server=socks5://127.0.0.1:9050']
+            args: ['--no-sandbox']
         });
 
         const page = await browser.newPage();
