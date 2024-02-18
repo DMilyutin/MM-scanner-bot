c3d4998ff6ef255cd8cc438cd57e2848bde1b80b
Dmitry Milyutin <dima.milyutin.1996@gmail.com>
2024-02-18 18:19:55 +0300

    Тест крона

--- src/modules/cron/cron.service.ts
+++ src/modules/cron/cron.service.ts
@@ -9,8 +9,8 @@ export class CronService {
   ){}
   
 
-  @Cron(CronExpression.EVERY_HOUR)
-  //@Cron('10 * * * * *')
+  //@Cron(CronExpression.EVERY_HOUR)
+  @Cron('10 * * * * *')
   async handleCron() { 
     console.log('Called every 10 seconds') 
     await this.scrapingService.updateProduct()
